0015b9118552564d762be7ee2532cf7e76312155
Gillian-Stephen <gillianstephen.1@gmail.com>
2022-11-16 13:20:22 -0400

    Created a bookcase app for React class 5

--- bookcase-app/src/App.js
+++ bookcase-app/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
 import Book from './components/Book';
 import data from './models/books.json';
 
 function App() {
-  const books = data;
-  return <Book/>;
+  const [books] = useState(data);
+  return books.map(book => <Book key={book.id} book ={book}/>);
+  // return <Book/>;
 }
 
 export default App;
