f191b78f225814e400001ca75e343d7244e5fd0e
Gillian-Stephen <gillianstephen.1@gmail.com>
2022-11-29 15:25:22 -0400

    Write car and other components for React 9

---
+++ build-a-car/src/components/Bicycle.js
@@ -0,0 +1,4 @@
+import React from 'react'
+export default function Bicycle() {
+ return <h2>Great, I am a Bicycle!</h2>;
+}
---
+++ build-a-car/src/components/Skateboard.js
@@ -0,0 +1,4 @@
+import React from 'react'
+export default function Car() {
+ return <h2>Watch me fly, I am a Skateboard!</h2>;
+}
